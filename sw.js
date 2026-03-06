6e8f7803ef729ed85d5008e6e79fb2388788b6a6
s.novoselov <s.novoselov@tinkoff.ru>
2026-03-06 11:23:37 +0300

    Add iOS26-style top bar PWA test page

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = "ios26-topbar-v1";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
